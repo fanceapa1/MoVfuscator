d41cc84e094fd8c72f650f5cbd41581f1a94320e
fanceapa1 <mosultudor@gmail.com>
2025-12-29 12:08:05 +0200

    Add div and idiv table generators

--- lookupTableGenerator.py
+++ lookupTableGenerator.py
@@ -49,6 +49,24 @@ def createImulHigherTable(size: int):
     table = [[(((i - 256 if i > 127 else i) * (j - 256 if j > 127 else j)) >> 8) % 256 for j in range(256)] for i in range(256)]
     return table
 
+def createDivTable(size: int):
+    table = [[(i // j) if j != 0 else 0 for j in range(256)] for i in range(256)]
+    return table
+
+def createModuloTable(size: int):
+    table = [[(i % j) if j != 0 else 0 for j in range(256)] for i in range(256)]
+    return table
+
+def createIdivTable(size: int):
+    S = lambda x: x - 256 if x > 127 else x
+    table = [[(int(S(i) / S(j)) % 256) if j != 0 else 0 for j in range(256)] for i in range(256)]
+    return table
+
+def createIModuloTable(size: int):
+    S = lambda x: x - 256 if x > 127 else x
+    table = [[(S(i) - int(S(i) / S(j)) * S(j)) & 0xFF if j != 0 else 0 for j in range(256)] for i in range(256)]
+    return table
+
 size = 256
 
 addTable = createAddTable(size)
@@ -59,7 +77,10 @@ incTable = createIncTable(size)
 incCarryTable = createIncCarryTable(size)
 decTable = createDecTable(size)
 decBorrowTable = createDecBorrowTable(size)
-mulLowerTable = createMulLowerTable(size)
+mulLowerTable = createMulLowerTable(size) # mul table = mul_lo + mul_hi
 mulHigherTable = createMulHigherTable(size)
 imulHigherTable = createImulHigherTable(size) # imul table = mul_lo + imul_hi
-
+divTable = createDivTable(size) # din fericire in probleme avem doar numere <256 si putem memora toate rezultatele
+moduloTable = createModuloTable(size)
+iDivTable = createIdivTable(size) # tabele diferite pentru impartirea cu semn
+iModuloTable = createIModuloTable(size)
